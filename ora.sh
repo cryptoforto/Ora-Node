ed26b06ea6ac0f1d54c500ec025f9b16f8150664
CryptoFortochka <131951520+cryptoforto@users.noreply.github.com>
2024-09-19 17:09:09 +0300

    ora.sh

--- ora.sh
+++ ora.sh
@@ -85,6 +85,8 @@ echo
 read -p "Enter your private key: " PRIV_KEY
 read -p "Enter your Alchemy WSS URL for Ethereum Mainnet: " MAINNET_WSS
 read -p "Enter your Alchemy HTTP URL for Ethereum Mainnet: " MAINNET_HTTP
+read -p "Enter your Alchemy WSS URL for Sepolia Ethereum: " SEPOLIA_WSS
+read -p "Enter your Alchemy HTTP URL for Sepolia Ethereum: " SEPOLIA_HTTP
 
 cat <<EOF > .env
 ############### Sensitive config ###############
